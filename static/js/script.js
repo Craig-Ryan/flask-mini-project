a4f0e7525af97de4055d110d3abed09a5330c65c
Craig Ryan <craig@craigryan.org>
2021-03-12 19:43:47 +0000

    Add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@
 // https://materializecss.com/sidenav.html#options
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"}); //places sidenav to the right
+    $('.collapsible').collapsible(); // Accordion
   });
