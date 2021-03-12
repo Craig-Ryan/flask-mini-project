4c050c109243cbabe36148bec1aeeaf4ad33ff80
Craig Ryan <craig@craigryan.org>
2021-03-12 11:31:50 +0000

    Added materialize navbar and sidenav

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Jquery that runs the mobile collapse navbar
+// https://materializecss.com/sidenav.html#options
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"}); //places sidenav to the right
+  });
