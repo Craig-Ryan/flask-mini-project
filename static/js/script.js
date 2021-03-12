b61777cf40d7cb348cc3704ccda3ea2a805e2676
Craig Ryan <craig@craigryan.org>
2021-03-12 20:58:44 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"}); //places sidenav to the right
     $('.collapsible').collapsible(); // Accordion
     $('.tooltipped').tooltip(); // https://materializecss.com/tooltips.html
+    $('.datepicker').datepicker({
+        format: "dd mmmm yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
